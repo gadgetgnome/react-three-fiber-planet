49a13f94c1b38515b726918da55bff760c0e885f
Pål Smitt-Amundsen <psa@try.no>
2022-11-24 21:50:28 +0100

    Tune stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
-
 import { useMemo, useRef } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import useScroll from "../utils/useScroll";
 import useMouse from "../utils/useMouse";
-
 
 function App() {
   const group = useRef();
@@ -16,24 +14,33 @@ function App() {
     const s = scroll.current;
 
     camera.position.y = -s * 30;
-    
-    const mouseDestX =  mouse.current * 2;
+
+    const mouseDestX = mouse.current * 2;
     camera.position.x += (mouseDestX - camera.position.x) / 20;
-    
-    group.current.children.forEach(c => {
-      c.rotation.x = scroll.current * 40;
+
+    group.current.children.forEach((c) => {
+      c.rotation.x = -scroll.current * 40;
+      c.rotation.y = state.clock.elapsedTime * 0.75;
     });
     window.state = state;
   });
 
   const primitives = useMemo(() => {
-    const randomPos =  () => {
-      const s = {x : 4, y: -50, z: -2};
-      return [ .1 + Math.random() * s.x, 5 + Math.random() * s.y, Math.random() * s.z];
-    }
+    const randomPos = () => {
+      const s = { x: 4, y: -50, z: -2 };
+      return [
+        0.1 + Math.random() * s.x,
+        5 + Math.random() * s.y,
+        Math.random() * s.z,
+      ];
+    };
     const a = new Array(50).fill().map((a, i) => (
       <mesh scale={1} position={randomPos()} key={i}>
-        { i % 2 === 0 ? <boxGeometry args={[1,1,1,4,4,4]}  /> : <sphereGeometry args={[.4]} />}
+        {i % 2 === 0 ? (
+          <boxGeometry args={[1, 1, 1, 4, 4, 4]} />
+        ) : (
+          <sphereGeometry args={[0.4, 16]} />
+        )}
         <meshStandardMaterial color={"green"} wireframe={true} />
       </mesh>
     ));
@@ -44,9 +51,7 @@ function App() {
     <>
       <directionalLight position={[0, -10, 0]} />
       <ambientLight intensity={0.2} />
-      <group ref={group}>
-        {primitives}
-      </group>
+      <group ref={group}>{primitives}</group>
     </>
   );
 }
